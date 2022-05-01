e5b1494ec394b70ecb1c5d081bdfb6bf81176ed7
alain17-web <web2020.alain@gmail.com>
2022-05-01 20:41:52 +0200

    compo intro

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Intro from './components/Intro';
 
 function App() {
   return (
     <div className='container'>
       <div className='yellow'></div>
       <div className='blue'></div>
+      <Intro />
     </div>
   );
 }
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 function Intro(){
     return (
-        <div>
-            
+        <div className='intro'>
+            <h1 className='title'>Quizzical</h1>
+            <p className='description'>Go ahead ! Do your worst and good luck!</p>
+            <button className='start'>Start Quizz</button>
         </div>
     )
 }
